cc0c108fcdb24b2d1160ad05c9942aa2b3a204b2
abderrazzak23 <abderrazzak23@gmail.com>
2023-10-27 18:10:05 -0700

    fix Betty error

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,7 +2,7 @@
 
 /**
  * main - function with two args
- * @argc: int argument , counter
+ * @argc: int argument  counter
  * @argv: char pointer
  * Return: 0
  */
@@ -10,6 +10,7 @@
 int main(int argc, char *argv[])
 {
 	int i;
+
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
