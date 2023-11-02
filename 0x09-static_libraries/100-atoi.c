f1518a811cc1360f40b0f2127006cf6bff96acb8
abderrazzak23 <abderrazzak23@gmail.com>
2023-11-01 19:32:51 -0700

    0x09-static_libraries

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,8 +1,33 @@
-#include "main.h"
-#include <stdio.h>
 /**
- * _atoi- convert char into integer
- * @s: char
- * Return: integer
+ * _atoi - Convert a string representation of an integer to an integer.
+ * @s: String possibly containing an integer.
+ *
+ * Return: The first integer in the string.
  */
-int _atoi(char *s);
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int sum = 0;
+	int result = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		if (*s == '-')
+			sign = -1;
+		else if (*s >= '0' && *s <= '9')
+			break;
+	}
+
+	while (*s >= '0' && *s <= '9')
+	{
+		sum = sum * 10 + (*s - '0');
+		s++;
+	}
+
+	if (sign == -1)
+		result = -sum;
+	else
+		result = sum;
+
+	return (result);
+}
