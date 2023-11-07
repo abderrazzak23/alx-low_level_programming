10e46eff8e01c0a9a506ad45e334c3c5b8626489
abderrazzak23 <abderrazzak23@gmail.com>
2023-11-06 18:11:35 -0800

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog struct
+ * @name: dog's name
+ * @age: dog age
+ * @owner: dog's owner name
+ *
+ * Description: header file
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
